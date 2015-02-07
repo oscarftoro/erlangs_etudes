db6f6f7297d0e9800d489bab3629ba6404d94e63
oscar_felipe <groovenino@gmail.com>
2015-02-07 18:13:48 +0100

    add guards to check number in etude3_3 in order to acomplish etude5_1

--- etude3_3.erl
+++ etude3_3.erl
@@ -18,13 +18,25 @@
 
 -spec(area(atom(), number(),number()) -> number()).
 
-area(rectangle, Side1,Side2) when Side1 > 0 andalso Side2 > 0  ->
-    Side1 * Side2;
-area(triangle,Side1,Side2) when Side1 > 0, Side2 > 0 ->
-    (Side1*Side2) /2;
-area(ellipse, MayorAxe,MinorAxe) when MayorAxe > 0, MinorAxe > 0 ->
-    math:pi()*MayorAxe*MinorAxe;
-area(_,_,_) -> 0. 
 
+area(rectangle, Side1,Side2) when Side1 > 0 andalso Side2 > 0,
+is_number(Side1), is_number(Side2)  ->
+    Side1 * Side2;
+area(triangle,Side1,Side2) when Side1 > 0, Side2 > 0,
+is_number(Side1), is_number(Side2) ->
+    (Side1*Side2) /2;
+area(ellipse, MayorAxe,MinorAxe) when MayorAxe > 0, MinorAxe > 0 , 
+is_number(MayorAxe), is_number(MinorAxe) ->
+    math:pi()*MayorAxe*MinorAxe;
+
+area(_,A,B) when A < 0 ; B < 0 -> 
+    io:format("Both numbers must be greater than or equal to zero.~n");
+area(_,A,_B) when is_number(A) =:= false ->
+    io:format("Error in first number. ~n");
+area(_,_A,B) when is_number(B) =:= false ->
+    io:format("Error in second number.~n");
+
+area(_,_,_) -> 
+     0.
  
 
