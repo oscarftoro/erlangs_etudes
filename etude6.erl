db1eb52abaa74147d66ac8adeec780f60976dd1c
oscar_felipe <groovenino@gmail.com>
2015-02-13 19:14:57 +0100

    add etude6_3

--- etude6.erl
+++ etude6.erl
@@ -1,5 +1,6 @@
 -module(etude6).
--export([minimum/1,maximum/1,range/1]).
+-export([minimum/1,maximum/1,range/1,julian/1]).
+-import(etude5_2,[date_parts/1]).
 
 -spec minimum([number()],[number()])-> number().
 minimum([],R) when R /= []->
@@ -30,9 +31,29 @@ maximum([H|T],R) when H > R ->
 maximum([_H|T],R) ->
     maximum(T,R).
 
+-spec range([number()]) ->[number()].
 range(L) ->
     [minimum(L),maximum(L)].
     
-    
+-spec julian(string()) -> integer().
+julian(S) ->
+   [Y,M,D] = date_parts(S),
+    case is_leap_year(Y) of
+	true -> February = 29;
+	false -> February = 28
+    end,
+    DaysPerMonth = [31, February, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+    julian(Y,M,D,DaysPerMonth,0).
+-spec julian(integer(),integer(),integer(),[integer()],integer()) ->
+integer().
+
+julian(Y,M,D,L,Acc) when M > (13 - length(L)) ->
+    julian(Y,M,D,tl(L),Acc + hd(L));
+julian(Y,M,D,L,Acc) when M =:= (13 - length(L)) ->
+    Acc + D.
+
+
+is_leap_year(Year) ->
+    (Year rem 4 == 0 andalso Year rem 100/= 0) orelse (Year rem 400 == 0).
     
     
