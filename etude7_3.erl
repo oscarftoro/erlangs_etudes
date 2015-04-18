0081b567a411de74edae058ff3ba939d97a495d4
oscar_felipe <groovenino@gmail.com>
2015-04-18 13:38:45 +0200

    add etude7_3.erl

--- etude7_3.erl
+++ etude7_3.erl
@@ -8,13 +8,13 @@ mean(L)->
     lists:foldl(fun(X,Acc) -> X + Acc end,0,L)/length(L).
 
 stdv_sums(Value,Acc) ->
-    [Sum, SumSquares] = Acc,
-    [Sum + Value, SumSquares + Value * Value].
+    {Sum, SumSquares} = Acc,
+    {Sum + Value, SumSquares + Value * Value}.
 
 -spec stdv([integer()]) -> float().
 
 stdv(Ls)-> 
     L = length(Ls),
-    [Sum,SquareSum] = lists:foldl(fun stdv_sums/2,[0,0],Ls),
+    {Sum,SquareSum} = lists:foldl(fun stdv_sums/2,{0,0},Ls),
     math:sqrt((L * SquareSum - Sum * Sum) / (L*(L - 1))).
 			 
